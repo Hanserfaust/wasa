22095eedfe4c87e10c155471c0782d8b194e3dde
Hanserfaust <hans.eklund@igw.se>
2020-11-08 18:49:35 +0100

    Removed hard dep on "http" on js script

--- webapp/js/wasa_back.js
+++ webapp/js/wasa_back.js
@@ -18,6 +18,7 @@ var WasaClient = function (hostname, port, game_session_id, username, game_event
 
     that.game_session_id = game_session_id;
     that.username = username;
+
     that.http_url = 'http://'+hostname+':'+port;
     that.ws_url = 'ws://'+hostname+':'+port;
 
